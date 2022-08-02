4b546f69be465a90334f86c48933c61393960625
Timo4D <79156877+Timo4D@users.noreply.github.com>
2022-08-02 15:42:08 +0200

    updated [id] to show court rating

--- pages/courts/[id].js
+++ pages/courts/[id].js
@@ -3,55 +3,109 @@ import styles from "../../styles/coutds.module.css";
 import Container from "react-bootstrap/Container";
 import CourtCarousel from "../../components/CourtCarousel";
 import dynamic from "next/dynamic";
-import { Col, Row } from "react-bootstrap";
+import { Button, Col, Form, Modal, Row } from "react-bootstrap";
 
 import React, { useState } from "react";
 
-import { PrismaClient, Prisma } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const MapWithNoSSR = dynamic(() => import("../../components/maps/Map"), {
     ssr: false,
 });
 
-const Courts = ({ res }) => {
+const Courts = ({ court }) => {
+    const [show, setShow] = useState(false);
+    const [rating, setRating] = useState(0);
     const router = useRouter();
     const { id } = router.query;
+
+    const handleShow = () => setShow(true);
+    const handleClose = () => setShow(false);
+    const handleSaveClose = () => {
+        handleClose();
+        console.log("Rating:" + rating);
+        //TODO: Handle the change in Rating
+    };
+    const handleRangeChange = (e) => {
+        setRating(e.target.value);
+    };
+
     return (
         <>
             <Container fluid className={styles.main}>
-                <h1 className={styles.title}>{res.name}</h1>
+                <div className={styles.title}>
+                    <h1>{court.name}</h1>
+                    <Button variant="primary" onClick={handleShow}>
+                        Rating: {court.rating}/10
+                    </Button>
+                </div>
 
                 <Container>
                     <CourtCarousel />
                     <h2>Beschreibung:</h2>
-                    <p className={styles.description}>Hallo {res.description}</p>
+                    <p className={styles.description}>
+                        Hallo {court.description}
+                    </p>
                     <hr />
                     <h2>Location</h2>
 
                     <div className={styles.map}>
                         <MapWithNoSSR
-                            lat={res.latitude}
-                            lng={res.longitude}
+                            lat={court.latitude}
+                            lng={court.longitude}
                             size={"25vw"}
                         />
                     </div>
                 </Container>
             </Container>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Rate {court.name}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group
+                            className="mb-3"
+                            controlId="exampleForm.ControlInput1"
+                        >
+                            <Form.Label>Rating</Form.Label>
+                            <Form.Range
+                                name="ratingRange"
+                                id="ratingRange"
+                                step={1}
+                                min="0"
+                                max="10"
+                                value={rating}
+                                onChange={handleRangeChange}
+                            />
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={handleClose} variant="secondary">
+                        Close
+                    </Button>
+                    <Button onClick={handleSaveClose} variant="primary">
+                        Save Rating
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </>
     );
 };
 
 export async function getServerSideProps(context) {
     const prisma = new PrismaClient();
-    const res = await prisma.court.findUnique({
+    const court = await prisma.court.findUnique({
         where: {
             id: parseInt(context.query.id),
         },
     });
-    console.log(res);
+    console.log(court);
     console.log("-------");
 
-    return { props: { res } };
+    return { props: { court } };
 }
 
 export default Courts;
