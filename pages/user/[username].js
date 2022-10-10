453739566564498cf197697d125100cbc62c7725
Timo4D <79156877+Timo4D@users.noreply.github.com>
2022-10-10 19:29:46 +0200

    Started to make edit profile work

--- pages/user/[username].js
+++ pages/user/[username].js
@@ -9,20 +9,37 @@ import { useSession } from "next-auth/react";
 const Account = ({ user }) => {
     const [posts, setPosts] = useState(true);
     const [courts, setCourts] = useState(true);
+    const [edit, setEdit] = useState(false);
+    const [editButtonText, setEditButtonText] = useState("Edit Profile");
+    const [bio, setBio] = useState("User Bio");
     const { data: session } = useSession();
+
+    function editProfile(event) {
+        setEdit(!edit);
+        if (edit) {
+            setEditButtonText("Edit Profile");
+            console.log("save");
+        } else {
+            setEditButtonText("Save Changes");
+        }
+        console.log("Edit profile " + edit);
+    }
 
     return (
         <Container className={styles.main}>
             <Row>
-                <Col fluid>
+                <Col>
                     <img
                         className={styles.profilePic}
                         alt="profile picture"
                         src={user.image}
                     />
                     <h2>{user.name}</h2>
-                    <p>User Bio</p>
-                    {session && user.name == session.user.name  && <Button>Edit Profile</Button>}
+                    {!edit && <p id="bio">{bio}</p>}
+                    {edit && <textarea className={styles.textArea} defaultValue={bio}></textarea>}
+                    {session && user.name == session.user.name && (
+                        <Button onClick={editProfile}>{editButtonText}</Button>
+                    )}
                 </Col>
 
                 <h3 onClick={() => setCourts(!courts)}>Main Courts</h3>
@@ -53,7 +70,6 @@ export async function getServerSideProps(context) {
             name: context.query.username,
         },
     });
-    //console.log(user);
     return { props: { user } };
 }
 
